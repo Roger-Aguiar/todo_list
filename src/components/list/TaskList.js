dbf976346341ee881171326eb8798b0f62a06594
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-18 15:25:28 -0300

    It creates a textbox to get the input from the user

--- src/components/list/TaskList.js
+++ src/components/list/TaskList.js
@@ -31,13 +31,25 @@ class TaskList extends Component
     render() 
     {
         return (
-            <div>                 
-                {this.state.taskList.map(newTask => 
-                (
-                    <li className = "listStyle" key = {newTask.id}>
-                        <input type = "checkbox"></input> {newTask.task}
-                    </li>
-                ))}                
+            
+            <div>        
+                <div>
+                    <h2>Enter a new task:</h2>
+                    
+                    <input type = "text" id = "newTask" name = "newTask" ></input>
+
+                    <button className = "buttonStyle" id = "taskButton">Add Task</button>               
+                </div>   
+                
+                <div>
+                    {this.state.taskList.map(newTask => 
+                    (
+                        <li className = "listStyle" key = {newTask.id}>
+                            <input type = "checkbox"></input> {newTask.task}
+                        </li>
+                    ))}    
+                </div>      
+                            
             </div>
         )
     }
